c68df7776c0c513d00a94f122523c5561cf34636
Shilpa <eclectic89@hotmail.com>
2024-09-19 11:19:27 +0100

    working API

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,29 @@ import { HfInference } from '@huggingface/inference'
 
 function App() {
   // new HfInference(process.env.token)
-const theToken= process.env.TOKEN
+const theToken= new HfInference("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")
 console.log(theToken)
-  return (
+
+const textToClassify = "I just bought a new camera. It's been a real disappointment."
+
+async function classifyText(){
+
+  try {
+    const response = await theToken.textClassification({
+          model: "SamLowe/roberta-base-go_emotions",
+          inputs: textToClassify
+      })
+      console.log(response[0].label)
+      console.log(response)
+  } catch (error){
+    console.log("Error is ", error)
+  }
+}
+classifyText()
+
+return (
     <>
-      <p>{theToken}</p>
+      
     </>
   )
 }
