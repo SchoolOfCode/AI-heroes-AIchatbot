18d16d56e320acfbce80cf34ad922ed3c666ca3d
rw4n99 <rowangodfrey2@gmail.com>
2024-09-19 12:01:25 +0100

    Added text to screen

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,39 @@
 
 import './App.css'
 import { HfInference } from '@huggingface/inference'
+import { useEffect, useState } from 'react'
 
 function App() {
   // new HfInference(process.env.token)
 const theToken= new HfInference("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")
-console.log(theToken)
 
-const textToClassify = "I just bought a new camera. It's been a real disappointment."
+const [result, setResult] = useState(null)
+
+const textToGenerate = "Whats is an api?"
 
 async function classifyText(){
 
   try {
-    const response = await theToken.textClassification({
-          model: "SamLowe/roberta-base-go_emotions",
-          inputs: textToClassify
+    const response = await theToken.textGeneration({
+          model: "HuggingFaceH4/zephyr-7b-beta",
+          inputs: textToGenerate,
       })
-      console.log(response[0].label)
+      setResult(response)
       console.log(response)
   } catch (error){
     console.log("Error is ", error)
   }
 }
-classifyText()
+
+useEffect(() => {
+  classifyText();
+}, []);
 
 return (
-    <>
-      
-    </>
-  )
-}
+  <div className="App">
+      <h1>Text Generation Result</h1>
+      {result ? <pre>{(result.generated_text)}</pre> : <p>Loading...</p>}
+    </div>
+)}
 
 export default App
